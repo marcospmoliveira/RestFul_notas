b7a6cb577fb82988b0d0645ff180947a4d7783ae
marcospmoliveira <marcospaulomeiradeoliveira@gmail.com>
2018-08-28 20:00:38 -0300

    first commit

---
+++ src/br/edu/devmedia/config/DBConfig.java
@@ -0,0 +1,13 @@
+package br.edu.devmedia.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConfig {
+
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/notes_db", "root", "my123");
+	}
+}
